d3f0a71729afc0238b4272d86361de1dee534e08
Artavazd Manukyan <ue4Artavazd256@outlook.com>
2021-03-30 19:03:38 +0400

    The following files have been updated/added:
    To crate client/server build scripts.

---
+++ ClientBuild.bat
@@ -0,0 +1,15 @@
+
+set UE4_PATH=C:\UnrealEngine\
+
+set PROJECT_NAME=zSpace
+
+set PROJECT_PATH=%CD%/%PROJECT_NAME%.uproject
+
+set CLIENTCONFIG=Development
+
+set ARCHIVEDIRECTORY=C:/%PROJECT_NAME%_Client/
+
+
+call "%UE4_PATH%\Engine\Build\BatchFiles\Build.bat" "%PROJECT_NAME%Editor" win64 %CLIENTCONFIG% "%PROJECT_PATH%" -WaitMutex
+
+call "%UE4_PATH%\Engine\Build\BatchFiles\RunUAT.bat" -ScriptsForProject="%PROJECT_PATH%" BuildCookRun -nocompileeditor -installed -nop4 -project="%PROJECT_PATH%" -cook -stage -archive -archivedirectory=%ARCHIVEDIRECTORY% -package -ue4exe="%UE4_PATH%\Engine\Binaries\Win64\UE4Editor-Cmd.exe"  -ddc=InstalledDerivedDataBackendGraph -pak -prereqs -nodebuginfo -targetplatform=Win64 -build -target=%PROJECT_NAME% -clientconfig=%CLIENTCONFIG% -utf8output
--- ServerBuild.bat
+++ ServerBuild.bat
@@ -7,10 +7,10 @@ set PROJECT_PATH=%CD%/%PROJECT_NAME%.uproject
 
 set CLIENTCONFIG=Development
 
-set ARCHIVEDIRECTORY=C:\Users\ue4Ar\Desktop\Server
+set ARCHIVEDIRECTORY=C:/zSpace_Server
 
  
-call "%UE4_PATH%\Engine\Build\BatchFiles\Build.bat" "%PROJECT_NAME%Editor" win64 Development "%PROJECT_PATH%" -WaitMutex
+call "%UE4_PATH%\Engine\Build\BatchFiles\Build.bat" "%PROJECT_NAME%Editor" win64 %CLIENTCONFIG% "%PROJECT_PATH%" -WaitMutex
 
 
-call "%UE4_PATH%\Engine\Build\BatchFiles\RunUAT.bat" -ScriptsForProject="%PROJECT_PATH%" BuildCookRun -project="%PROJECT_PATH%" -noP4 -clientconfig=%CLIENTCONFIG% -serverconfig=%CLIENTCONFIG% -nocompileeditor -ue4exe="%UE4_PATH%\Engine\Binaries\Win64\UE4Editor-Cmd.exe"  -utf8output -server -serverplatform=Win64 -noclient -build -cook -Maps=List+Of+Maps+To+Include -compressed -stage -package -stagingdirectory=%ARCHIVEDIRECTORY% -compile
+call "%UE4_PATH%\Engine\Build\BatchFiles\RunUAT.bat" -ScriptsForProject="%PROJECT_PATH%" BuildCookRun -nocompileeditor -nop4 -project="%PROJECT_PATH%" -cook -stage -archive -archivedirectory=%ARCHIVEDIRECTORY% -package -ue4exe="%UE4_PATH%\Engine\Binaries\Win64\UE4Editor-Cmd.exe" -ddc=DerivedDataBackendGraph -pak -prereqs -nodebuginfo -targetplatform=Win64 -build -target=zSpaceServer -serverconfig=%CLIENTCONFIG% -utf8output -compile
