e91584170331d8f80e3836aa0fceef24d94a1856
Artavazd Manukyan <ue4Artavazd256@outlook.com>
2021-08-26 13:08:54 +0400

    Added rectify input action and add unit test.

---
+++ Source/zSpace/Tests/SandBoxTests/SandBoxTests.cpp
@@ -0,0 +1,12 @@
+// Copyright 2020 Sabre Dart Studios
+
+
+#include "Tests/SandBoxTests/SandBoxTests.h"
+
+SandBoxTests::SandBoxTests()
+{
+}
+
+SandBoxTests::~SandBoxTests()
+{
+}
---
+++ Source/zSpace/Tests/SandBoxTests/SandBoxTests.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sabre Dart Studios
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class ZSPACE_API SandBoxTests
+{
+public:
+	SandBoxTests();
+	~SandBoxTests();
+};
